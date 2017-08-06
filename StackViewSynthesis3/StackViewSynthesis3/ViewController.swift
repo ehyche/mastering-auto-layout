68c75d09a390f072351739df526c07a2da666f90
Eric Hyche <ehyche@groupon.com>
2017-08-06 19:49:16 -0400

    Remove solution code from main ViewController.swift

--- StackViewSynthesis3/StackViewSynthesis3/ViewController.swift
+++ StackViewSynthesis3/StackViewSynthesis3/ViewController.swift
@@ -34,73 +34,7 @@ class ViewController: UIViewController {
         stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
         stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
 
-        // Configure the stack view here
-        stackView.axis = .vertical
-        stackView.distribution = .fill
-        stackView.alignment = .fill
-        stackView.spacing = 4.0
-
-        // Add an "Add Item" row to the stack view
-        let addItemButton = UIButton(frame: .zero)
-        addItemButton.setTitle("Add Item", for: .normal)
-        addItemButton.setTitleColor(UIColor.blue, for: .normal)
-        addItemButton.addTarget(self, action: #selector(addItemButtonTapped(sender:)), for: .touchUpInside)
-
-        stackView.addArrangedSubview(addItemButton)
-    }
-
-    @objc private func addItemButtonTapped(sender: UIButton) {
-        let insertionIndex = stackView.arrangedSubviews.count - 1
-        let viewToInsert = rowView()
-        viewToInsert.isHidden = true
-        stackView.insertArrangedSubview(viewToInsert, at: insertionIndex)
-
-        UIView.animate(withDuration: 0.3, animations: {
-            viewToInsert.isHidden = false
-        })
-    }
-
-    @objc private func deleteItemButtonTapped(sender: UIButton) {
-        if let viewToRemove = sender.superview,
-           stackView.arrangedSubviews.contains(viewToRemove) {
-            UIView.animate(withDuration: 0.3,
-                           animations: {
-                viewToRemove.isHidden = true
-            },
-                           completion: { (finished) in
-                viewToRemove.removeFromSuperview()
-            })
-        }
-    }
-
-    private func rowView() -> UIView {
-        let rowStackView = UIStackView(frame: .zero)
-
-        rowStackView.axis = .horizontal
-        rowStackView.distribution = .fill
-        rowStackView.alignment = .firstBaseline
-        rowStackView.spacing = 8.0
-
-        let dateLabel = UILabel(frame: .zero)
-        dateLabel.text = DateFormatter.localizedString(from: Date(), dateStyle: .none, timeStyle: .short)
-        dateLabel.font = UIFont.preferredFont(forTextStyle: .body, compatibleWith: traitCollection)
-        dateLabel.textColor = UIColor.black
-
-        let uuidLabel = UILabel(frame: .zero)
-        uuidLabel.text = UUID().uuidString
-        uuidLabel.textColor = UIColor.black
-        uuidLabel.font = UIFont.preferredFont(forTextStyle: .headline, compatibleWith: traitCollection)
-
-        let deleteButton = UIButton(frame: .zero)
-        deleteButton.setTitle("Delete", for: .normal)
-        deleteButton.setTitleColor(UIColor.blue, for: .normal)
-        deleteButton.addTarget(self, action: #selector(deleteItemButtonTapped(sender:)), for: .touchUpInside)
-
-        rowStackView.addArrangedSubview(dateLabel)
-        rowStackView.addArrangedSubview(uuidLabel)
-        rowStackView.addArrangedSubview(deleteButton)
-
-        return rowStackView
+        // Add your code here
     }
 
 
