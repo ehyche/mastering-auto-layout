289b1ec2e8b4831b99dc69ff31a12a4c79919b17
Eric Hyche <ehyche@groupon.com>
2017-08-10 23:40:45 -0400

    Add view controller delegate methods

--- LearnAboutSizeClasses/LearnAboutSizeClasses/ViewController.swift
+++ LearnAboutSizeClasses/LearnAboutSizeClasses/ViewController.swift
@@ -8,14 +8,25 @@
 
 import UIKit
 
+extension UIUserInterfaceSizeClass {
+    var debugText: String {
+        switch self {
+            case .compact: return "Compact"
+            case .regular: return "Regular"
+            case .unspecified: return "Unspecified"
+        }
+    }
+}
+
+extension UITraitCollection {
+
+    var debugText: String {
+        return "(H:\(horizontalSizeClass.debugText),V:\(verticalSizeClass.debugText))"
+    }
+}
+
 class ViewController: UIViewController {
 
-    @IBOutlet weak var stackView: UIStackView!
-    @IBOutlet weak var horizontalSizeClassLabel: UILabel!
-    @IBOutlet weak var verticalSizeClassLabel: UILabel!
-    @IBOutlet weak var compactWidthLabel: UILabel!
-    @IBOutlet weak var regularWidthLabel: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -26,6 +37,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        print("Will Transition To Trait Collection \(newCollection.debugText)")
+    }
+
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        print("Will Transition to Size \(size)")
+    }
+
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        print("Trait Collection Did Change. New Trait Collection = \(traitCollection.debugText)")
+    }
 
 }
 
