e766e87f9260618b07fd10e2ee588ae1cb991a58
Eric Hyche <ehyche@groupon.com>
2017-08-13 11:34:25 -0600

    Put the svaffolding code back into Viewcontroller.swift

--- StackViews/Synthesis/ProfileForm/StackViewSynthesis2/ViewController.swift
+++ StackViews/Synthesis/ProfileForm/StackViewSynthesis2/ViewController.swift
@@ -10,10 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let rootStackView = UIStackView(frame: .zero)
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Add your code here
+        view.backgroundColor = UIColor.white
+
+        // Set up the root stack view
+        rootStackView.translatesAutoresizingMaskIntoConstraints = false
+        rootStackView.axis = .vertical
+        rootStackView.alignment = .fill
+        rootStackView.spacing = 8.0
+
+        // Add it to the view hierarchy and anchor it
+        view.addSubview(rootStackView)
+        rootStackView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor).isActive = true
+        rootStackView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor).isActive = true
+        rootStackView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 20.0).isActive = true
+        bottomLayoutGuide.topAnchor.constraint(equalTo: rootStackView.bottomAnchor, constant: 20.0).isActive = true
+
+        // Add stuff to the stack view here.
 
 
     }
